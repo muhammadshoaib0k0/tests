70bf64e34a733b2a067f9eae8632c39e48600f00
Muhammad Shoaib <muhammadshoaib0k0@gmail.com>
2022-05-15 16:04:07 +0500

    user-model-and-its-crud-functionaly

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ApplicationRecord
   has_many :collaborations, dependent: :destroy
   has_many :repositories, dependent: :destroy
   has_many :shared_repositories, through: :collaborations, source: :repository
+
+  validates :email, presence: true
+
+  validates :first_name, presence: true, length: { in: 2..15 }
+  validates :last_name, presence: true, length: { in: 2..15 }
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
